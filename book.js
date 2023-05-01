dc764ac6c8cb29287d9530f04e1d86fc9049bfeb
porag-m06 <porag.m06@gmail.com>
2023-05-01 20:30:55 +0600

    linters issue fixed

--- book.js
+++ book.js
@@ -1,48 +1,46 @@
+/* eslint-disable no-use-before-define */
 const bookCollection = [];
 const showBks = document.querySelector('#show-bks');
 const newBk = document.querySelector('#new-bk');
 
 function AddBook(bookTitle, bookAuthor) {
-    bookCollection.push({ title: bookTitle, author: bookAuthor });
-}//AddBook
+  bookCollection.push({ title: bookTitle, author: bookAuthor });
+}// AddBook
 
 function ShowAllBooks() {
-    showBks.innerHTML =``;
+  showBks.innerHTML = '';
 
-    for(let i = 0; i < bookCollection.length; i+=1){
-        const addedBk = document.createElement('div');
-        addedBk.className = 'added-bks';
-        addedBk.id = `${i}`
-        addedBk.innerHTML = `
+  for (let i = 0; i < bookCollection.length; i += 1) {
+    const addedBk = document.createElement('div');
+    addedBk.className = 'added-bks';
+    addedBk.id = `${i}`;
+    addedBk.innerHTML = `
             <p class="title">${bookCollection[i].title}</p>
             <p class="author">${bookCollection[i].author}</p> 
             <button id="del-bk">Remove</button>`;
-        showBks.appendChild(addedBk);
-    }
+    showBks.appendChild(addedBk);
+  }
 
-    const removeBtns = document.querySelectorAll(".added-bks");
-    removeBook(removeBtns);
-    
-}//ShowAllBooks
+  const removeBtns = document.querySelectorAll('.added-bks');
+  RemoveBook(removeBtns);
+}// ShowAllBooks
 
-function removeBook(removeBtns) {
-    removeBtns.forEach((btn)=>{
-        btn.addEventListener('click',()=>{
-            bookCollection.splice(btn.getAttribute('id'), 1);
-            ShowAllBooks();
-        })
-    })
-}//removeBook
+function RemoveBook(removeBtns) {
+  removeBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      bookCollection.splice(btn.getAttribute('id'), 1);
+      ShowAllBooks();
+    });
+  });
+}// removeBook
 
 newBk.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const bookTitle = document.querySelector('#bk-title').value;
-    const bookAuthor = document.querySelector('#bk-author').value;
-    AddBook(bookTitle, bookAuthor);
-    ShowAllBooks();
+  e.preventDefault();
+  const bookTitle = document.querySelector('#bk-title').value;
+  const bookAuthor = document.querySelector('#bk-author').value;
+  AddBook(bookTitle, bookAuthor);
+  ShowAllBooks();
 });
-
-
 
 // preserve data in local storage
 
@@ -56,12 +54,12 @@ const userBkData = { bktitle: '', bkauthor: '' };
 // const setUserBkData = localStorage.setItem('userBkData', JSON.stringify(userBkData));
 
 userBkInputs.forEach((input) => {
-    input.addEventListener(('change', () => {
-        // e.preventDefault();
-        userBkData.bktitle = bkTitle.value;
-        userBkData.bkauthor = bkAuthor.value;
-        localStorage.setItem('userBkData', JSON.stringify(userBkData));
-    }));
+  input.addEventListener(('change', () => {
+    // e.preventDefault();
+    userBkData.bktitle = bkTitle.value;
+    userBkData.bkauthor = bkAuthor.value;
+    localStorage.setItem('userBkData', JSON.stringify(userBkData));
+  }));
 });
 
 // set data
